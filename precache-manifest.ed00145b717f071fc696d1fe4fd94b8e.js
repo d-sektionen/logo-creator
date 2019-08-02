75a8c4af8bfcf0ccc2a7f1dffdb20711f066471a
Emil <emil19@users.noreply.github.com>
2019-08-02 18:11:53 +0200

    Updates

---
+++ precache-manifest.ed00145b717f071fc696d1fe4fd94b8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5098f2705e4d7bd8017436d374fa3fb",
+    "url": "/logo-creator/index.html"
+  },
+  {
+    "revision": "e5f86bca02f62f1164ae",
+    "url": "/logo-creator/static/css/main.7d55c055.chunk.css"
+  },
+  {
+    "revision": "5c9ff16ec5bee154aee2",
+    "url": "/logo-creator/static/js/2.5286435d.chunk.js"
+  },
+  {
+    "revision": "e5f86bca02f62f1164ae",
+    "url": "/logo-creator/static/js/main.3cd36b19.chunk.js"
+  },
+  {
+    "revision": "cce14f0eaafd61eb28ad",
+    "url": "/logo-creator/static/js/runtime~main.e17fe58b.js"
+  }
+]);
